8ade340e1f81b67f122eb8ff09d63cc27e8b6fc0
liuyan <2841955648@qq.com>
2017-10-31 13:21:45 +0800

    添加任务调度页面

--- src/main/java/com/iscas/sdas/controller/TimerController.java
+++ src/main/java/com/iscas/sdas/controller/TimerController.java
@@ -1,13 +1,34 @@
 package com.iscas.sdas.controller;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.iscas.sdas.util.CommonUntils;
 
 @Controller
 @RequestMapping("/timer")
 public class TimerController {
 	@RequestMapping("/task")
 	public ModelAndView getdispplay(){
+		return new ModelAndView("/task/task");
+	}
+	@RequestMapping("/section")
+	public ModelAndView section(HttpServletRequest request){
+		String  source=request.getParameter("source");
+		String  output=request.getParameter("output");
+		String  ifdel=request.getParameter("ifdel");
+		Boolean ifdeleteOut=false;
+		if(!CommonUntils.isempty(ifdel)){
+			if(ifdel.equals("1")){
+				ifdeleteOut=true;
+			}else{
+				ifdeleteOut=false;
+			}
+		}
+		if(!CommonUntils.isempty(source)&&!CommonUntils.isempty(output)&&!CommonUntils.isempty(ifdel)){
+		}
 		return new ModelAndView("/task/task");
 	}
 }
---
+++ src/main/webapp/js/task.js
@@ -0,0 +1,37 @@
+/**
+ * by ly 2017年10月31日下午14:00:31
+ */
+function section(){
+	var Source=$("#Source").val();
+	var Output=$("#Output").val();
+	var ifDel=$("input[name='deleteOut']:checked").val();
+	$.ajax({
+		url:"/sdas/timer/",
+		data:{
+			"source":Source,
+			"output":Output,
+			"ifdel":ifDel
+		},
+		daraType:"post",
+		success:function(data){
+			alert("切片成功！");
+			//还原
+			$("#Source").val("");
+			$("#Output").val("");
+			$(":radio[name='deleteOut'][value='1']").prop("checked", "checked");
+		}
+	});
+}
+function calculate(id,url,areaId){
+	var time=$(id).val();
+	$.ajax({
+		url:url,
+		data:{
+			"time":time
+		},
+		daraType:"post",
+		success:function(data){
+			
+		}
+	});
+}
